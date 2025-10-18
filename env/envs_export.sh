1478b2bc005b4c3b8a3195f6b6519c06bbdb5f96
Micah Killian <micah@killianarts.online>
2025-10-19 07:51:12 +0900

    Added envs.

---
+++ env/envs_export.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+export MYSQL_DATABASE="kyoenji_wordpress"
+export MYSQL_USER="kyoenji_admin"
+export MYSQL_PASSWORD="12345"
+export MYSQL_ROOT_PASSWORD="password"
+export WORDPRESS_DB_HOST="db:3306"
+export WORDPRESS_DB_USER="kyoenji_admin"
+export WORDPRESS_DB_PASSWORD="12345"
+export WORDPRESS_DB_NAME="kyoenji_wordpress"
